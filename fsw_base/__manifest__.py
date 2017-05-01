9558c21e4eec160e9a57cfe56433e8232590c388
estelleesparon <estelleesparon@192.168.1.81>
2017-04-30 15:12:17 -1000

    [ADD]fsw_base: Ajout du menu Sociétés

--- fsw_base/__manifest__.py
+++ fsw_base/__manifest__.py
@@ -14,11 +14,17 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Sample',
+    'category': 'Paramètres techniques',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
+    
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+
 
     'qweb': [
         "static/src/xml/*.xml",
