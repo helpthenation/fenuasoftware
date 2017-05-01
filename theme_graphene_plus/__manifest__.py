05fd287b8d6de3868129c5f3b9f9679381467237
estelleesparon <estelleesparon@192.168.1.81>
2017-04-30 14:25:19 -1000

    [ADD]theme_graphene_plus: Ajout nouveau module

---
+++ theme_graphene_plus/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Graphene +",
+
+    'summary': """
+    """,
+
+    'description': """
+    """,
+
+    'author': "Fenua Software",
+    'website': "http://www.fenuasoftware.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Theme/Corporate',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['theme_graphene'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/assets.xml',
+        'views/options.xml',
+    ],
+}
