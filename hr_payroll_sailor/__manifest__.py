6add66c6338fbb91e60d81497b73360cfa7979d1
Heifara MATAPO <heifaramatapo@gmail.com>
2017-07-21 11:54:09 -1000

    [UPDATE] hr_payroll_sailor : Mise à jour des vues
    Dans les lignes de factures , ajouter une colonne Campagne de pêche.
    Implémenter la vue pour une campagne de pêche

--- hr_payroll_sailor/__manifest__.py
+++ hr_payroll_sailor/__manifest__.py
@@ -23,8 +23,12 @@ Gestion de la paie des marins en Polynésie Française - En accord avec le fichi
 
     # always loaded
     'data': [
+        #security
         'security/ir.model.access.csv',
-        'views/views.xml',
+
+        #views
+        'views/account_invoice_view.xml',
+        'views/fishing_campaign.xml',
         'views/templates.xml',
     ],
 }
