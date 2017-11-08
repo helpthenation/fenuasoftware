e678670cd47844b972885aa9ecf6da43e2959195
Heifara MATAPO <heifaramatapo@gmail.com>
2017-11-07 15:43:59 -1000

    [ADD] fsw_base : Ajout du module

---
+++ fsw_base/models/res_config_settings.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    example = fields.Boolean(text='Un exemple')
+    module_sample = fields.Boolean("Installe le module sample")
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        res.update(
+            example=self.env['ir.config_parameter'].sudo().get_param('fsw_base.example'),
+        )
+        return res
+
+    @api.multi
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('fsw_base.example', self.example)
