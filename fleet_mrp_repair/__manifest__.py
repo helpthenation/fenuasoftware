9748e6d39eb92d3d93eef46d5b39c4b736510b19
Heifara MATAPO <heifaramatapo@gmail.com>
2017-11-16 10:40:39 -1000

    [UPDATE] fleet_mrp_repair : mise à jour de la description

--- fleet_mrp_repair/__manifest__.py
+++ fleet_mrp_repair/__manifest__.py
@@ -5,6 +5,7 @@
     Link between Fleet and MRP Repair
     """,
     'description': """
+    Ajoute le véhicule dans la fiche atelier et sur la facture.
     """,
     'author': "Fenua Softw@re",
     'website': "http://www.fenuasoftware.com",
