2df5c020e879d7616822baf6711b6329a2e406af
Heifara MATAPO <heifaramatapo@gmail.com>
2017-11-15 14:24:03 -1000

    [UPDATE] fsw_base : Changer Odoo en FenuaSoftware

--- fsw_base/__manifest__.py
+++ fsw_base/__manifest__.py
@@ -9,9 +9,11 @@
     'website': "https://fenuasoftware.com/",
     'category': 'Technical Settings',
     'version': '0.1',
-    'depends': ['calendar'],
+    'depends': ['base', 'mail', 'calendar', 'auth_signup'],
     'data': [
+        'views/mail_template_views.xml',
         'views/res_config_settings_views.xml',
+        'views/webclient_templates.xml',
     ],
     'qweb': [
         "static/src/xml/*.xml",
