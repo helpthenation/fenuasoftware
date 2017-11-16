09696e9ed1d8899fb9db5b5cfd705cbf085305ce
Heifara MATAPO <heifaramatapo@gmail.com>
2017-11-15 16:04:29 -1000

    [ADD] website_plus : Ajout du module

---
+++ website_plus/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Builder +",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "Fenua Softw@re",
+    'website': "https://www.fenuasoftware.com",
+    'category': 'Website',
+    'version': '0.1',
+    'depends': ['website'],
+    'data': [
+        'views/website_templates.xml',
+        'views/website_views.xml',
+    ],
+    'auto_install': True,
+}
