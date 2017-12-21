197c145a77d6d710d25fc19783e899265f24796c
Heifara MATAPO <heifaramatapo@gmail.com>
2017-12-20 14:09:21 -1000

    [ADD] calendar_plus : Ajoute du module

---
+++ calendar_plus/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Calendar +",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "Fenua Software",
+    'website': "https://www.fenuasoftware.com",
+    'category': 'Extra Tools',
+    'version': '0.1',
+    'depends': ['calendar'],
+    'data': [
+        'views/calendar_views.xml',
+    ],
+    'auto_install': True
+}
