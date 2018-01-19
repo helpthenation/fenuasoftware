be1f15467c0047248e407b0161da3189c89051b0
Heifara MATAPO <heifaramatapo@gmail.com>
2018-01-18 14:22:49 -1000

    [ADD] account_invoicing_plus : Ajout du module
    Ajoute le menu Comptabilité dans Invoicing.

---
+++ account_invoicing_plus/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Invoicing Management +",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "Fenua Software",
+    'website': "https://www.fenuasoftware.com",
+    'category': 'Invoicing Management',
+    'version': '0.1',
+    'depends': ['account_invoicing'],
+    'data': [
+        'views/account_invoicing_plus_views.xml',
+        'security/security.xml',
+    ],
+    'auto_install': True
+}
