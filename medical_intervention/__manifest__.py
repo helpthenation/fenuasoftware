8cc2c553b71274259e0b9b2548cc510dac4b1f03
Heifara MATAPO <heifaramatapo@gmail.com>
2018-02-15 19:53:43 -1000

    [ADD] medical_intervention : Ajout du module
    Début de codage du module avec fonction et vue basique

---
+++ medical_intervention/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Intervention Médicale",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Themes/Backend',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/medical_intervention_views.xml',
+        'views/res_partner_views.xml',
+
+        'views/medical_intervention_reports.xml',
+    ],
+}
