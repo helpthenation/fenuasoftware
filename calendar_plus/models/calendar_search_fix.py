d4bcb66b8e2ba606a95a2f91b26d5d13dd6725d4
Heifara MATAPO <heifaramatapo@gmail.com>
2018-02-22 13:36:33 -1000

    [FIX] calendar_plus : Ajouter la méthode _fixargs qui enlève l'argument start

--- calendar_plus/models/calendar_search_fix.py
+++ calendar_plus/models/calendar_search_fix.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+import logging
 
 from odoo import models, fields, api
+
+from odoo.addons.calendar.models.calendar import get_real_ids
+
+_logger = logging.getLogger(__name__)
 
 
 class Meeting(models.Model):
@@ -12,6 +17,17 @@ class Meeting(models.Model):
         Corriger un bug dans le calendrier lorsqu'on est en affichage par Jour, tous les événements ne s'affichent pas.
         Un ticket à été remonté sur le repos d'Odoo : [11.0] Calendar Event not showing in Calendar's Day Mode #23246
         """
-        args = []
+        self._fixargs(args)
+        _logger.info("search(" + str(args) + ")")
         res = super(Meeting, self).search(args, offset, limit, order, count)
         return res
+
+    def _fixargs(self, args):
+        """
+        Méthode qui enlève l'argument start car génère un bug d'affichage. Probablement due au Timezone.
+        :param args: liste d'arguement à traiter
+        :customer concerns: Cyrille SERRA
+        """
+        for arg in args:
+            if 'start' in arg:
+                args.remove(arg)
