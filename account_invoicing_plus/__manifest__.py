3a97fdf7b99b216f19ace5348a7318fbcd19ce24
Heifara MATAPO <heifaramatapo@gmail.com>
2018-03-08 10:53:40 -1000

    [ADD] account_invoicing_plus : Ajout d'une zone de config

--- account_invoicing_plus/__manifest__.py
+++ account_invoicing_plus/__manifest__.py
@@ -11,8 +11,8 @@
     'version': '0.1',
     'depends': ['account_invoicing'],
     'data': [
+        'views/res_config_settings_views.xml',
         'views/account_invoicing_plus_views.xml',
         'security/security.xml',
     ],
-    'auto_install': True
 }
