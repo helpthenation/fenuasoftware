ea64c6b63a427a8a74236d2bfa93ad1434e31b78
Heifara MATAPO <heifaramatapo@gmail.com>
2018-03-10 09:02:30 -1000

    [FIX] fsw_base : Corriger les dépendances.

--- fsw_base/__manifest__.py
+++ fsw_base/__manifest__.py
@@ -9,12 +9,13 @@
     'website': "https://fenuasoftware.com/",
     'category': 'Technical Settings',
     'version': '0.1',
-    'depends': ['base', 'mail', 'calendar', 'auth_signup'],
+    'depends': ['base', 'mail', 'calendar', 'board'],
     'data': [
         'views/mail_template_views.xml',
         'views/mail_templates.xml',
         'views/res_config_settings_views.xml',
         'views/webclient_templates.xml',
+        'data/functions_after_installation.xml',
     ],
     'qweb': [
         "static/src/xml/*.xml",
