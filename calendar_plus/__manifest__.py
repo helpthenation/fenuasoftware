3ca3887edd64503ae46978c334a27efb526c692f
Heifara MATAPO <heifaramatapo@gmail.com>
2018-03-16 17:00:49 -1000

    [FIX] calendar_plus : Supprimer la zone de config par erreur

--- calendar_plus/__manifest__.py
+++ calendar_plus/__manifest__.py
@@ -11,6 +11,7 @@
     'version': '0.1',
     'depends': ['calendar'],
     'data': [
+        'views/res_config_settings_views.xml',
         'views/calendar_template.xml',
         'views/calendar_views.xml',
     ],
