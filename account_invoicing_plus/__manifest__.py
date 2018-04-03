4a242df9b4fed3a1e189ed4d9cc3165e26d86fd0
Heifara MATAPO <heifaramatapo@gmail.com>
2018-04-02 20:30:15 -1000

    [ADD] account_invoicing_plus : Ajout d'import QIF avec corrections sur encodage

--- account_invoicing_plus/__manifest__.py
+++ account_invoicing_plus/__manifest__.py
@@ -9,10 +9,11 @@
     'website': "https://www.fenuasoftware.com",
     'category': 'Invoicing Management',
     'version': '0.1',
-    'depends': ['account_invoicing'],
+    'depends': ['account_invoicing', 'account_bank_statement_import', 'base_import'],
     'data': [
         'views/res_config_settings_views.xml',
         'views/account_invoicing_plus_views.xml',
+        'wizard/account_bank_statement_import_views.xml',
         'security/security.xml',
     ],
 }
