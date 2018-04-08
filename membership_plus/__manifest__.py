6786a57cb9f61ae8d353c99132903a8c12d54d97
Heifara MATAPO <heifaramatapo@gmail.com>
2018-04-07 17:18:05 -1000

    [ADD] membership_plus : Ajout du module avec implémentations

---
+++ membership_plus/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Membership Management +",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "Fenua Software",
+    'website': "https://www.fenuasoftware.com",
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['membership'],
+    'data': [
+        'reports/membership_card_template.xml',
+        'reports/membership_card_report.xml',
+        'views/membership_invoice_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/templates.xml',
+        'wizard/membership_check_wizard.xml',
+    ],
+}
---
+++ membership_plus/models/model.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+import random
+from odoo import api, fields, models, _
+
+
+class MembershipInvoice(models.TransientModel):
+    _inherit = "membership.invoice"
+
+    date_from = fields.Date(string='From')
+    date_to = fields.Date(string='To')
+
+    @api.multi
+    def membership_invoice(self):
+        if self:
+            datas = {
+                'membership_product_id': self.product_id.id,
+                'amount': self.member_price,
+                'date_from': self.date_from,
+                'date_to': self.date_to,
+            }
+        invoice_list = self.env['res.partner'].browse(self._context.get('active_ids')).create_membership_invoice(datas=datas)
+
+        search_view_ref = self.env.ref('account.view_account_invoice_filter', False)
+        form_view_ref = self.env.ref('account.invoice_form', False)
+        tree_view_ref = self.env.ref('account.invoice_tree', False)
+
+        return {
+            'domain': [('id', 'in', invoice_list)],
+            'name': 'Membership Invoices',
+            'res_model': 'account.invoice',
+            'type': 'ir.actions.act_window',
+            'views': [(tree_view_ref.id, 'tree'), (form_view_ref.id, 'form')],
+            'search_view_id': search_view_ref and search_view_ref.id,
+        }
+
+
+class MembershipLine(models.Model):
+    _inherit = 'membership.membership_line'
+
+    date_from = fields.Date(string='From', readonly=False)
+    date_to = fields.Date(string='To', readonly=False)
+
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    membership_barcode = fields.Char()
+
+    def generate_membership_barcode(self):
+        self.membership_barcode = "".join(random.sample("abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?", 12))
+        print(self.membership_barcode)
+
+    @api.multi
+    def create_membership_invoice(self, product_id=None, datas=None):
+        invoice_list = super(Partner, self).create_membership_invoice(product_id, datas)
+        for invoice in invoice_list:
+            for invoice_line in self.env['account.invoice'].browse([invoice]).invoice_line_ids:
+                member_lines = self.env['membership.membership_line'].search([('account_invoice_line', '=', invoice_line.id)])
+                if member_lines:
+                    for member_line in member_lines:
+                        member_line.date_from = datas.get('date_from')
+                        member_line.date_to = datas.get('date_to')
+
+        return invoice_list
