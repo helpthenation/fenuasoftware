03c8227d96b063ce3f47c26880abaa11cc197631
Heifara MATAPO <heifaramatapo@gmail.com>
2018-04-26 13:28:01 -1000

    [FIX] calendar_plus : Le propriétaire n'est plus administrateur, mais bien l'employée

--- calendar_plus/models/calendar_event.py
+++ calendar_plus/models/calendar_event.py
@@ -8,6 +8,20 @@ class CalendarEvent(models.Model):
 
     partner_details = fields.Char(compute='_get_partner_details', string='Attendees')
 
+    @api.model
+    def create(self, values):
+        if 'appointment_type_id' in values:
+            for item in values.get('partner_ids'):
+                # item = ([0,id,False])
+                # item[1] return partner's id
+                user = self.env['res.users'].search([('partner_id', '=', item[1])])
+                if user:
+                    values['user_id'] = user.id
+                    break;
+
+        event = super(CalendarEvent, self).create(values)
+        return event
+
     @api.multi
     def _get_partner_details(self):
         for event in self:
