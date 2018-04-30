140bd10ae4e3e781a56f96017597d6b210e1e476
Heifara MATAPO <heifaramatapo@gmail.com>
2018-04-30 07:13:13 -1000

    [ADD] account_invoicing_plus : Ajout de la config pour installer account_move_template

--- account_invoicing_plus/models/__init__.py
+++ account_invoicing_plus/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import account, models
+from . import account, res_config_settings
---
+++ account_invoicing_plus/models/res_config_settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    module_account_move_template = fields.Boolean("Installe le module")
