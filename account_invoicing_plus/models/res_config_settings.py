6540e406d3bbdbd26e9feaf28e402ec31a17a0f7
Heifara MATAPO <heifaramatapo@gmail.com>
2018-05-01 15:16:10 -1000

    [UPDATE] account_invoicing_plus : Description de la configuration pour le modèle de pièce comptable

--- account_invoicing_plus/models/res_config_settings.py
+++ account_invoicing_plus/models/res_config_settings.py
@@ -6,4 +6,4 @@ from odoo import api, fields, models, _
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    module_account_move_template = fields.Boolean("Installe le module")
+    module_account_move_template = fields.Boolean("Modèle de Pièce Comptable")
