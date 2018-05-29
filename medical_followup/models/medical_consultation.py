e04ef01eecdf6b4d2740190177c6cf554e86092f
Heifara MATAPO <heifaramatapo@gmail.com>
2018-05-29 09:14:28 -1000

    [UPDATE] medical_followup : Ajouter compte rendu opératoire

--- medical_followup/models/medical_consultation.py
+++ medical_followup/models/medical_consultation.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+import time
+from datetime import datetime, timedelta
+from dateutil import relativedelta
 
-from odoo import models, fields, api, _
+import babel
+
+from odoo import api, fields, models, tools, _
 
 
 class Partner(models.Model):
@@ -21,6 +26,31 @@ class MedicalConsultation(models.Model):
     def _default_date(self):
         return fields.Date.context_today(self)
 
+    name = fields.Char(readonly=True, compute="_compute_name")
     date = fields.Date(string="Date de consultation médicale", default=_default_date)
     patient = fields.Many2one('res.partner', string="Patient")
     description = fields.Text(string="Description")
+    user_id = fields.Many2one('res.users', compute="_compute_user_id")
+
+    type = fields.Selection([
+        ('crop', 'Compte Rendu Opératoire'),
+    ], string="Type", default=None,
+        help="Détermine si c'est un type particulier.")
+    operator = fields.Many2one('res.partner', string="Opérateur")
+    anesthetist = fields.Many2one('res.partner', string="Anesthésiste")
+    reason = fields.Char(string="Motif d'hospitalisation")
+
+    @api.onchange('type', 'date')
+    def _compute_name(self):
+        if self.type == 'crop':
+            self.name = "COMPTE RENDU OPERATOIRE"
+        else:
+            locale = self.env.context.get('lang') or 'en_US'
+            ttyme = datetime.fromtimestamp(time.mktime(time.strptime(self.date, "%Y-%m-%d")))
+            self.name = "Intervention du " + tools.ustr(babel.dates.format_date(date=ttyme, format='dd MMMM y', locale=locale))
+
+    def _compute_user_id(self):
+        self.user_id = self.env.user
+
+    def _get_printed_report_name(self):
+        return "INTERVENTIONS_MEDICAL"
