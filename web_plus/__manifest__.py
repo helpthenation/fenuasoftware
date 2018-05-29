623226b6195720407f5bf6125005f252b56660a8
Heifara MATAPO <heifaramatapo@gmail.com>
2018-05-28 15:11:54 -1000

    [ADD] web_plus : Ajout du module

---
+++ web_plus/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Web +",
+    'summary': """
+    Surcharge du module web qui gère la partie client d'Odoo.
+    """,
+    'description': """
+    """,
+    'author': "Fenua Softw@re",
+    'website': "https://www.fenuasoftware.com",
+    'version': '0.1',
+    'depends': ['web'],
+    'data': [
+        'views/report_templates.xml',
+    ],
+}
