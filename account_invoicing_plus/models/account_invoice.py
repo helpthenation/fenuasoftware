3a875d116123455b47e91275efff70606138e526
Heifara MATAPO <heifaramatapo@gmail.com>
2018-06-11 15:59:48 -1000

    [FIX] account_invoicing_plus : Corriger bug si date est False

--- account_invoicing_plus/models/account_invoice.py
+++ account_invoicing_plus/models/account_invoice.py
@@ -15,6 +15,7 @@ class AccountInvoice(models.Model):
 
     @api.depends('date_invoice')
     def _compute_period(self):
-        locale = self.env.context.get('lang') or 'en_US'
-        ttyme = datetime.fromtimestamp(time.mktime(time.strptime(self.date_invoice, "%Y-%m-%d")))
-        self.period = tools.ustr(babel.dates.format_date(date=ttyme, format='MMMM y', locale=locale))
+        if self.date_invoice:
+            locale = self.env.context.get('lang') or 'en_US'
+            ttyme = datetime.fromtimestamp(time.mktime(time.strptime(self.date_invoice, "%Y-%m-%d")))
+            self.period = tools.ustr(babel.dates.format_date(date=ttyme, format='MMMM y', locale=locale))
