2da6b559223dcd497aedb173681e2d7d2fcd9092
Heifara MATAPO <heifaramatapo@gmail.com>
2018-07-20 09:24:24 -1000

    [ADD] point_of_sale_plus : Ajouter bouton pour désactiver le bouton Bande Z

--- point_of_sale_plus/models/res_config_settings.py
+++ point_of_sale_plus/models/res_config_settings.py
@@ -7,6 +7,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     module_point_of_sale_fiscal_position_with_fixed_selling_price = fields.Boolean("Point of Sale Fiscal Position With Fixed Selling Price")
+    module_point_of_sale_disable_print_sale_details = fields.Boolean("Point of Sale Disable Print Sale Details")
 
     @api.model
     def get_values(self):
