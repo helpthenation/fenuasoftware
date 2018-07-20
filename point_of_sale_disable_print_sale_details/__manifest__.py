67e6abbda1ca6d15abe0c4642031f29281c5e94d
Heifara MATAPO <heifaramatapo@gmail.com>
2018-07-20 09:23:51 -1000

    [ADD] point_of_sale_disable_print_sale_details : Ajout du module.

---
+++ point_of_sale_disable_print_sale_details/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Point of Sale Disable Print Sale Details",
+    'summary': """
+    Le bouton d'impression des détails de vente (bande Z) est désactiver.
+    """,
+    'description': """
+    Le bouton d'impression des détails de vente (bande Z) est désactiver.
+    """,
+    'author': "Fenua Softw@re",
+    'website': "https://www.fenuasoftware.com",
+    'category': 'Point of Sale/Tools',
+    'version': '0.1',
+    'depends': ['point_of_sale_plus'],
+    'qweb': ['static/src/xml/pos.xml'],
+}
