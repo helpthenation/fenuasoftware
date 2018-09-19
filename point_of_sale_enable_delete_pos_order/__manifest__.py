7bb5f2ab1eef39db7131e3c381a03c5c274ba204
Heifara MATAPO <heifaramatapo@gmail.com>
2018-09-18 17:44:04 -1000

    [ADD] point_of_sale_enable_delete_pos_order : Ajout du module.

---
+++ point_of_sale_enable_delete_pos_order/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Point of Sale Enable Delete POS Orders",
+    'summary': """
+    """,
+    'description': """
+    """,
+    'author': "Fenua Softw@re",
+    'website': "https://www.fenuasoftware.com",
+    'category': 'Point of Sale',
+    'version': '0.1',
+    'depends': ['point_of_sale_plus'],
+    'data': [
+        'views/pos_session_view.xml',
+    ],
+}
--- point_of_sale_plus/models/res_config_settings.py
+++ point_of_sale_plus/models/res_config_settings.py
@@ -8,6 +8,7 @@ class ResConfigSettings(models.TransientModel):
 
     module_point_of_sale_fiscal_position_with_fixed_selling_price = fields.Boolean("Point of Sale Fiscal Position With Fixed Selling Price")
     module_point_of_sale_disable_print_sale_details = fields.Boolean("Point of Sale Disable Print Sale Details")
+    module_point_of_sale_enable_delete_pos_order = fields.Boolean("Point of Sale Enable Delete POS Orders")
 
     @api.model
     def get_values(self):
