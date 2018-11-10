813458c54ab8bc69943cd3d535020586ba5ce5a9
Heifara MATAPO <heifaramatapo@gmail.com>
2018-11-10 10:57:23 -1000

    [REM] sale_order_html_documentation : Supprimer module

--- sale_order_html_documentation/models/sale_order.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    html_documents = fields.Many2many(comodel_name='html.document')
