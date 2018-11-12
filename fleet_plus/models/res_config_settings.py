956c53531eb651c8287d7beaac1860ff24a418b4
Heifara MATAPO <heifaramatapo@gmail.com>
2018-11-11 16:03:39 -1000

    [MIG] fleet_plus : Migrer module

--- fleet_plus/models/res_config_settings.py
+++ fleet_plus/models/res_config_settings.py
@@ -8,16 +8,3 @@ class ResConfigSettings(models.TransientModel):
 
     module_fleet_contacts = fields.Boolean('Liée parc automobile avec les contacts')
     module_fleet_mrp_repair = fields.Boolean('Liée parc automobile avec la réparation')
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res.update(
-            example=self.env['ir.config_parameter'].sudo().get_param('fsw_base.example'),
-        )
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param('fsw_base.example', self.example)
