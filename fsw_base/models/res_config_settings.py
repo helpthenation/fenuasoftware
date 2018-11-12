f63cfef34bdc72e80c93d416d294e7151de8cdb0
Heifara MATAPO <heifaramatapo@gmail.com>
2018-11-11 15:52:04 -1000

    [UPDATE] fsw_base : Ajouter zone de config pour point_of_sale_plus

--- fsw_base/models/res_config_settings.py
+++ fsw_base/models/res_config_settings.py
@@ -11,6 +11,7 @@ class ResConfigSettings(models.TransientModel):
     module_contacts_plus = fields.Boolean("Contact+")
     module_crm_plus = fields.Boolean("CRM+")
     module_l10n_pf = fields.Boolean("Comptabilité Polynésie Française")
+    module_point_of_sale_plus = fields.Boolean("Point de Vente+")
     module_purchase_plus = fields.Boolean("Achats+")
     module_repair_plus = fields.Boolean("Repair+")
     module_sale_management_plus = fields.Boolean("Ventes+")
@@ -19,6 +20,5 @@ class ResConfigSettings(models.TransientModel):
 
     #NOT MIGRATE
     module_l10n_pf_hr_payroll = fields.Boolean("Paie Polynésie Française")
-    module_point_of_sale_plus = fields.Boolean("Point de Vente+")
     module_fleet_plus = fields.Boolean("Parc Automobile+")
     module_membership_plus = fields.Boolean("Adhérent+")
