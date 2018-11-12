400f46909f5a1f0911d72c182ebcf284af409b24
Heifara MATAPO <heifaramatapo@gmail.com>
2018-11-11 20:20:24 -1000

    [FIX] contacts_plus : Corriger warning sur facebook_url_enable

--- contacts_plus/models/res_partner.py
+++ contacts_plus/models/res_partner.py
@@ -11,7 +11,6 @@ class ResPartner(models.Model):
     facebook_url = fields.Char(string="Facebook")
     facebook_url_enable = fields.Boolean(compute='_compute_facebook_url_enable', default=False, readonly=False)
 
-    @api.depends('facebook_url_enable')
     @api.one
     def _compute_facebook_url_enable(self):
         self.facebook_url_enable = self.env['ir.config_parameter'].sudo().get_param('contacts_plus.facebook_url_enable')
