4b626fa53cc34acf22f4836dccd423d1b8258077
Heifara MATAPO <heifaramatapo@gmail.com>
2018-11-11 20:34:21 -1000

    [UPDATE] calendar_plus : Ajouter paramètre show_attendees_details

--- calendar_plus/models/res_config_settings.py
+++ calendar_plus/models/res_config_settings.py
@@ -7,12 +7,14 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     module_web_calendar_config_mac5 = fields.Boolean("Web Calendar Configuration")
+    show_attendees_details = fields.Boolean("Masque les photos des participants et affiche des détails à la place")
     hide_user_in_calendar_event = fields.Boolean("Masquer utilisateur dans les événements de calendrier ")
 
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         res.update(
+            show_attendees_details=self.env['ir.config_parameter'].sudo().get_param('calendar_plus.show_attendees_details'),
             hide_user_in_calendar_event=self.env['ir.config_parameter'].sudo().get_param('calendar_plus.hide_user_in_calendar_event'),
         )
         return res
@@ -20,4 +22,8 @@ class ResConfigSettings(models.TransientModel):
     @api.multi
     def set_values(self):
         super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('calendar_plus.show_attendees_details', self.show_attendees_details)
+        view = self.env.ref('calendar_plus.view_calendar_event_display_form')
+        view.update({'active': self.show_attendees_details})
+
         self.env['ir.config_parameter'].sudo().set_param('calendar_plus.hide_user_in_calendar_event', self.hide_user_in_calendar_event)
