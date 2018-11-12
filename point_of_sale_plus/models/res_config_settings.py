126f932a2c74f473ab34e766148e5ead47b2d22e
Heifara MATAPO <heifaramatapo@gmail.com>
2018-11-11 15:56:14 -1000

    [MIG] point_of_sale_plus : Migrer module

--- point_of_sale_plus/models/res_config_settings.py
+++ point_of_sale_plus/models/res_config_settings.py
@@ -9,16 +9,3 @@ class ResConfigSettings(models.TransientModel):
     module_point_of_sale_fiscal_position_with_fixed_selling_price = fields.Boolean("Point of Sale Fiscal Position With Fixed Selling Price")
     module_point_of_sale_disable_print_sale_details = fields.Boolean("Point of Sale Disable Print Sale Details")
     module_point_of_sale_enable_delete_pos_order = fields.Boolean("Point of Sale Enable Delete POS Orders")
-
-    @api.model
-    def get_values(self):
-        res = super(ResConfigSettings, self).get_values()
-        res.update(
-            example=self.env['ir.config_parameter'].sudo().get_param('fsw_base.example'),
-        )
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param('fsw_base.example', self.example)
