a83e2bef34bcdd39e52bd374bb1df90424c42373
Heifara MATAPO <heifaramatapo@gmail.com>
2018-11-27 21:01:47 -1000

    [ADD] medical_followup : Ajouter l'impression du formulaire médical

--- medical_followup/__manifest__.py
+++ medical_followup/__manifest__.py
@@ -15,6 +15,7 @@
         'security/ir.model.access.csv',
         'views/medical_followup_view.xml',
         'views/medical_information_document.xml',
+        'views/medical_information_document_template.xml',
         'views/medical_consultation_views.xml',
         'views/medical_consultation_template.xml',
         'views/prescription_views.xml',
