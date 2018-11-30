049acb27f74cdd3ce5acc28cc2fe07ec3d1f9dd5
Heifara MATAPO <heifaramatapo@gmail.com>
2018-11-29 20:47:40 -1000

    [FIX] account_invoicing_plus : Corriger impression du champ période.

--- account_invoicing_plus/__manifest__.py
+++ account_invoicing_plus/__manifest__.py
@@ -8,14 +8,14 @@
     'author': "Fenua Software",
     'website': "https://www.fenuasoftware.com",
     'category': 'Invoicing Management',
-    'version': '0.1',
+    'version': '0.2',
     'depends': ['fsw_base', 'account', 'base_import'],
     'data': [
         # 'data/account_payment_action_server.xml',
         'views/res_config_settings_views.xml',
         'views/account_invoicing_plus_views.xml',
         'views/account_payment_view.xml',
-        # 'views/report_invoice.xml',
+        'views/report_invoice.xml',
         'security/security.xml',
     ],
 }
